bd0a92ac383c6c0764a5aa793cc3ad129511fa4c
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2024-08-27 19:31:22 -0300

    feat: change controller to receive base64 image

--- src/http/controllers/measures/create-measure-controller.ts
+++ src/http/controllers/measures/create-measure-controller.ts
@@ -1,40 +1,61 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { pipeline } from 'node:stream'
-import { promisify } from 'node:util'
+
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 import { makeCreateMeasureUseCase } from '@/use-cases/measures/factories/make-create-measure-use-case'
-import { generateUniqueFilename } from '@/utils/generate-unique-filename'
-import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-
-const pump = promisify(pipeline)
 
 export const createMeasureController: FastifyPluginAsyncZod = async (app) => {
-  app.post('/', {}, async (request, reply) => {
-    const data = await request.file()
+  app.post(
+    '/',
+    {
+      schema: {
+        body: z.object({
+          image: z.string(),
+          customer_code: z.string(),
+          measure_type: z.enum(['WATER', 'GAS']),
+          measure_datetime: z.coerce.date(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { image, customer_code, measure_type, measure_datetime } =
+        request.body
 
-    if (!data) {
-      return reply.status(400).send({
-        error_code: 'INVALID_DATA',
-        error_description: 'Missing image input.',
+      const [dataSchema, base64] = image.split(',')
+      const mimeMatch = dataSchema.match(/^data:image\/(png|jpg|jpeg);base64$/)
+
+      if (!mimeMatch) {
+        return reply.status(400).send({
+          error_code: 'INVALID_DATA',
+          error_description:
+            'Image must be in base64 format with PNG, JPEG, or JPG type.',
+        })
+      }
+
+      const mimeType = mimeMatch[1]
+      const extension = mimeType === 'jpeg' ? 'jpg' : mimeType
+      const timestamp = new Date(measure_datetime).getTime()
+      const filename = `${customer_code}-${measure_type.toLowerCase()}-${timestamp}.${extension}`
+
+      const destination = path.resolve(__dirname, '../../../../tmp', filename)
+      const buffer = Buffer.from(base64, 'base64')
+
+      await fs.promises.writeFile(destination, buffer)
+
+      const file = new File([buffer], filename)
+
+      const createMeasureUseCase = makeCreateMeasureUseCase()
+
+      const result = await createMeasureUseCase.execute({
+        image: file,
+        customer_code,
+        measure_datetime,
+        measure_type,
       })
-    }
 
-    const filename = generateUniqueFilename(data.filename)
-
-    const destination = path.resolve(__dirname, '../../../../tmp', filename)
-
-    await pump(data.file, fs.createWriteStream(destination))
-
-    const createMeasureUseCase = makeCreateMeasureUseCase()
-
-    const result = await createMeasureUseCase.execute({
-      image: new File([await data.toBuffer()], filename),
-      customer_code: '1234',
-      measure_datetime: new Date(),
-      measure_type: 'WATER',
-    })
-
-    return reply.send(result)
-  })
+      return reply.send(result)
+    },
+  )
 }
--- src/utils/generate-unique-filename.ts
+++ src/utils/generate-unique-filename.ts
@@ -1,10 +1,7 @@
-import { randomUUID } from 'node:crypto'
-import path from 'node:path'
-
-export function generateUniqueFilename(filename: string) {
-  const extension = path.extname(filename)
-
-  const baseName = path.basename(filename, extension)
-
-  return `${baseName}-${randomUUID()}${extension}`
+export function generateUniqueFilename(
+  customerCode: string,
+  measureType: 'WATER' | 'GAS',
+  extension: string,
+) {
+  return `${customerCode}-${measureType}${extension}`
 }
